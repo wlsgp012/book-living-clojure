d8b50c346d40d69ac602c7bf7a4c5d40672f877e
jinhye <jinhye.kim@navercorp.com>
2019-05-16 22:10:53 +0900

    Add files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to book-living-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/book_living_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns book-living-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [book-living-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
